9b7729a2ae70770af8b0172bfad38824c8f977c5
Aliaksandr Prakhotski <a.prakhotski@gmail.com>
2020-12-25 15:41:14 +0300

    Final edit of filter2.0

--- public/assets/framework/js/main.js
+++ public/assets/framework/js/main.js
@@ -1,27 +1,33 @@
-filterTracks('singer');
-filterTracks('genre');
-filterTracks('year');
+	
+let tracks = $('tr[data-visible]');
 
-function filterTracks(item){
-
-    $('.filter-' + item + '').on('change', function(e) {
-
-        let value = e.target.value;
-
-        let tbody = $('#filter-list');
-
-        if (value.length) {
-
-            tbody.find('tr[data-' + item + '!="' + value + '"]').hide();
-            tbody.find('tr[data-' + item + '="' + value + '"]').show();
-        } else {
-
-            tbody.find('tr').show();
-        }
-    });
-}
-
-
+$('.form-filter').on('change', function(){
+	let 
+		selectedSinger = $('.filter-singer').val();
+		selectedGenre = $('.filter-genre').val();
+		selectedYear = $('.filter-year').val();
+		
+		$.each(tracks,function(index,row){
+		
+			row.dataset.visible = 'shown';
+			
+			if(
+				row.cells[0].innerHTML.indexOf(selectedSinger) == -1 || 
+				row.cells[2].innerHTML.indexOf(selectedGenre) == -1 ||
+				row.cells[3].innerHTML.indexOf(selectedYear) == -1
+				
+			){
+				row.dataset.visible = 'hidden';
+				console.log(row.cells[2].innerHTML.indexOf(selectedGenre));
+				$("tr[data-visible='shown']").show();
+				$("tr[data-visible='hidden']").hide();
+			}else{
+				row.dataset.visible = 'shown';
+				$("tr[data-visible='shown']").show();
+			}
+				
+		});			
+});
+   
 
 new Tablesort(document.getElementById('table-id'));
-
